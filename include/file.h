94ea25a56b9c208a43f9227c80ad2649fe834642
aarl2934 <aarl2934@stthomas.edu>
2021-05-11 12:07:10 -0700

    added file.h

---
+++ include/file.h
@@ -0,0 +1,19 @@
+#ifndef __FILE_H_
+#define __FILE_H_
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+class File{
+private:
+    SDL_Texture *icon;
+    TTF_Font *font;
+    SDL_Texture *phrase;
+public:
+    
+
+
+
+}
+
+
+#endif
